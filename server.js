7725ae00ee8f8859393037dc73b4e6473ef589ce
lexi-winstanley <50344054+lexi-winstanley@users.noreply.github.com>
2019-08-17 12:45:01 -0700

    not to be pushed space changes in server.js

--- server.js
+++ server.js
@@ -55,10 +55,9 @@ app.get("/api/waitlist", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
     var newTables = req.body;
-  
+
     // Using a RegEx Pattern to remove spaces from newCharacter
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newTables.routeName = newTables.name.replace(/\s+/g, "").toLowerCase();
   
     console.log(newTables);
   
