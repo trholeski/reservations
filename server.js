64221a131aafbf0fa03b40de39c4973d1516bb53
Tim Holeski <trholeski@gmail.com>
2019-08-17 11:59:44 -0700

    updated server.js to include post logic

--- server.js
+++ server.js
@@ -44,6 +44,36 @@ app.get("/api/waitlist", function(req, res) {
     return res.json(waitList);
   });
 
+  app.post("/api/tables", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newTables = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newTables.routeName = newTables.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newTables);
+  
+    tables.push(newCharacter);
+  
+    res.json(newTables);
+  });
+  app.post("/api/waitlist", function(req, res) {
+      // req.body hosts is equal to the JSON post sent from the user
+      // This works because of our body parsing middleware
+      var newWaitlist = req.body;
+    
+      // Using a RegEx Pattern to remove spaces from newCharacter
+      // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+      newWaitlist.routeName = newWaitlist.name.replace(/\s+/g, "").toLowerCase();
+    
+      console.log(newWaitlist);
+    
+      waitlist.push(newWaitlist);
+    
+      res.json(newWaitlist);
+    });
 // Starts the server to begin listening
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
